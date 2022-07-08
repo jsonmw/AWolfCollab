1add029035cbbd0b238a6056e018fd3da31f86af
Jason W <jasonmwild@gmail.com>
2022-07-07 18:34:23 -0700

    hailSatan

--- Nothing/assets/scripts/app.js
+++ Nothing/assets/scripts/app.js
@@ -1,7 +1,17 @@
+// We should change the name of this to "main.js" or whatever.
 
 const greetButton = document.getElementById('greet');
 
 greetButton.addEventListener('click', greetingsFromJimbo);
+
+// how to get Jim to turn into satan on mouseover? click?
+
+// greetButton.addEventListener('mouseover', hailSatan);
+
+// function hailSatan() {
+//     const image = document.querySelector('img');
+//     image.innerHTML="<img src='https://pbs.twimg.com/media/DQJRFgsW4AUKvZd.jpg:large'>";
+// }
 
 function greetingsFromJimbo() {
     const name = document.getElementById('name').value;
