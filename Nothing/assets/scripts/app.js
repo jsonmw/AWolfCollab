ccb286ca273891e9d7962f1f3ba5e9a065f2495e
jasonw <jasonmwild@gmail.com>
2022-07-08 15:08:38 -0700

    working on the main page

--- Nothing/assets/scripts/app.js
+++ Nothing/assets/scripts/app.js
@@ -1,17 +1,25 @@
 // We should change the name of this to "main.js" or whatever.
 
 const greetButton = document.getElementById('greet');
-
 greetButton.addEventListener('click', greetingsFromJimbo);
 
-// how to get Jim to turn into satan on mouseover? click?
+const goodJim = document.getElementById('good-jim');
+const badJim = document.getElementById('bad-jim');
+const mainHeader = document.getElementById('main-header');
+goodJim.addEventListener('mouseover', hailSatan);
+badJim.addEventListener('mouseover', hailSatan);
 
-// greetButton.addEventListener('mouseover', hailSatan);
-
-// function hailSatan() {
-//     const image = document.querySelector('img');
-//     image.innerHTML="<img src='https://pbs.twimg.com/media/DQJRFgsW4AUKvZd.jpg:large'>";
-// }
+function hailSatan() {
+    if(goodJim.className === 'jim-visible') {
+        goodJim.className = 'jim-invisible';
+        badJim.className = 'jim-visible';
+        mainHeader.innerText = "LORD SATAN";
+    } else {
+        goodJim.className = 'jim-visible';
+        badJim.className = 'jim-invisible';
+        mainHeader.innerText = "JIMBO OYLE";
+    }
+}
 
 function greetingsFromJimbo() {
     const name = document.getElementById('name').value;
