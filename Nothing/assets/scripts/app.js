935624486821feb37e27d80f6a360071aeb28ba2
jasonw <jasonmwild@gmail.com>
2022-07-08 16:40:14 -0700

    tweaks

--- Nothing/assets/scripts/app.js
+++ Nothing/assets/scripts/app.js
@@ -1,38 +1,41 @@
 // We should change the name of this to "main.js" or whatever.
 
-const greetButton = document.getElementById('greet');
-greetButton.addEventListener('click', greetingsFromJimbo);
+const greetButton = document.getElementById("greet");
+greetButton.addEventListener("click", greetingsFromJimbo);
 
-const goodJim = document.getElementById('good-jim');
-const badJim = document.getElementById('bad-jim');
-const mainHeader = document.getElementById('main-header');
-const menuHeader = document.getElementById('menu-header');
-const listItems = document.querySelectorAll('li'); // returns an object containing all list items that you can reference by index
-goodJim.addEventListener('mouseover', hailSatan);
-badJim.addEventListener('mouseover', hailSatan);
+const goodJim = document.getElementById("good-jim");
+const badJim = document.getElementById("bad-jim");
+const mainHeader = document.getElementById("main-header");
+const menuHeader = document.getElementById("menu-header");
+const mainContent = document.getElementById("main-content");
+const listItems = document.querySelectorAll("li"); // returns an object containing all list items that you can reference by index
+goodJim.addEventListener("mouseover", hailSatan);
+badJim.addEventListener("mouseover", hailSatan);
 
 // Swaps Jimbo for Satan.
 
 function hailSatan() {
-    if(goodJim.className === 'jim-visible') {
-        goodJim.className = 'jim-invisible';
-        badJim.className = 'jim-visible';
-        mainHeader.innerText = "LORD SATAN";
-        menuHeader.innerText = "SATAN";
-        listItems[2].innerText = "Meet Satan";
-    } else {
-        goodJim.className = 'jim-visible';
-        badJim.className = 'jim-invisible';
-        mainHeader.innerText = "JIMBO OYLE";
-        menuHeader.innerText = "JIMBO";
-        listItems[2].innerText = "Meet Jim";
-    }
+  if (goodJim.className === "jim-visible") {
+    goodJim.className = "jim-invisible";
+    badJim.className = "jim-visible";
+    mainHeader.innerText = "LORD SATAN";
+    menuHeader.innerText = "SATAN";
+    listItems[2].innerText = "Meet Satan";
+    mainContent.className = "main-content-hell";
+  } else {
+    goodJim.className = "jim-visible";
+    badJim.className = "jim-invisible";
+    mainHeader.innerText = "JIMBO OYLE";
+    menuHeader.innerText = "JIMBO";
+    listItems[2].innerText = "Meet Jim";
+    mainContent.className = "main-content";
+  }
 }
 
 // Renders greeting based on given input text.
 
 function greetingsFromJimbo() {
-    const name = document.getElementById('name').value;
-    const greetingSpace = document.getElementById('greeting');
-    greetingSpace.innerText =`hello ${name}, I am jimbo`;
+  const name = document.getElementById("name").value;
+  const greetingSpace = document.getElementById("greeting");
+  greetingSpace.innerText = `hello ${name}, I am jimbo`;
 }
--- Nothing/assets/scripts/bullshit.js
+++ Nothing/assets/scripts/bullshit.js
@@ -5,25 +5,25 @@ let ageOfBill = 2.9;
 let a = 69;
 let b = 420;
 
-const bill = (a, b) =>  a + b;
+const bill = (a, b) => a + b;
 
 class Helper {
-    static helperCount;
-    static capitalize(string) {
-        return string.toUpperCase();        
-    }
-    
-    constructor() {
-        this.helperCount++;
-        // this.addHelper();
-    }
+  static helperCount;
+  static capitalize(string) {
+    return string.toUpperCase();
+  }
 
-    // addHelper(){
-    // }
+  constructor() {
+    this.helperCount++;
+    // this.addHelper();
+  }
 
-    getHelperCount() {
-        return this.helperCount;
-    }
+  // addHelper(){
+  // }
+
+  getHelperCount() {
+    return this.helperCount;
+  }
 }
 
 const helper1 = new Helper();
@@ -31,58 +31,59 @@ const helper2 = new Helper();
 const helper3 = new Helper();
 const helper4 = new Helper();
 
-const array1 = [1,2,3];
+const array1 = [1, 2, 3];
 const array2 = array1;
 
-
-console.log(helper4.getHelperCount())
-console.log(helper2.getHelperCount())
+console.log(helper4.getHelperCount());
+console.log(helper2.getHelperCount());
 
 class Bill {
-    billAge = 2.99;
-    billTreat;
+  billAge = 2.99;
+  billTreat;
 
-    
-    constructor(color){
-        this.billColor = color;
-    }
-    
-    setTreatType(shape, color, taste, smell) {
-        this.billTreat = 'dank'
-    }
-    eat() {
-        alert(`Warning: A ${this.billAge.toFixed(1)} year old dog is eating ${this.billTreat} treats` )
-    }
-    returnBillObj() {
-        const billObject = {
-            color: 'gold',
-            age: 2.9,
-            eat() {
-                console.log("YUM");
-            },
-            favTreats: ['crunchy', 'stinky', 'bison lung', {treat: 'Inner treat'}],
-            innerObject: {billGuts: 'yuck'}
-        }
+  constructor(color) {
+    this.billColor = color;
+  }
 
-        this.returnBillObj;
-    }
+  setTreatType(shape, color, taste, smell) {
+    this.billTreat = "dank";
+  }
+  eat() {
+    alert(
+      `Warning: A ${this.billAge.toFixed(1)} year old dog is eating ${
+        this.billTreat
+      } treats`
+    );
+  }
+  returnBillObj() {
+    const billObject = {
+      color: "gold",
+      age: 2.9,
+      eat() {
+        console.log("YUM");
+      },
+      favTreats: ["crunchy", "stinky", "bison lung", { treat: "Inner treat" }],
+      innerObject: { billGuts: "yuck" },
+    };
 
-    returnBillStats() {
-        return {height: 3, weight: 82}
-    }
-    // sum(...array) {
-    //     let sum = 0;
+    this.returnBillObj;
+  }
 
-    //     for (n of array) {
-    //         sum +=n;
-    //     }
+  returnBillStats() {
+    return { height: 3, weight: 82 };
+  }
+  // sum(...array) {
+  //     let sum = 0;
 
-    
-    //     console.log(sum)
-    // }
+  //     for (n of array) {
+  //         sum +=n;
+  //     }
+
+  //     console.log(sum)
+  // }
 }
 
-const array = [1,2,3,4,5,6,7,8,9,10];
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // class Jake extends Bill {
 //     constructor(eyesight) {
 //         super(color);
@@ -94,20 +95,17 @@ const array = [1,2,3,4,5,6,7,8,9,10];
 
 // console.log(actualJake.eyesight);
 
-
 // const actualBill = new Bill('gold');
 
 // actualBill.setTreatType();
 // actualBill.eat();
-    // actualBill.sum(array);
-
+// actualBill.sum(array);
 
 // console.log(bill(a,b));
 // const catType = 'trash';
 // function ruckus (catType,) {
 //     console.log(`My cat Ruckus is a ${catType} type.`);
 // }
-
 
 // const bill2 = () {
 //     console.log(`
@@ -117,6 +115,6 @@ const array = [1,2,3,4,5,6,7,8,9,10];
 
 // }
 
-let stringNumber = '67';
+let stringNumber = "67";
 let sum = +stringNumber + 2;
-console.log(sum)
+console.log(sum);
