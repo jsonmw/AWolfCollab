0a3c7016969a69819470075821f6de6546063e06
Jason W <jasonmwild@gmail.com>
2022-07-15 15:17:20 -0700

    blackjack

---
+++ Nothing/assets/scripts/blackjack.js
@@ -0,0 +1,33 @@
+
+
+const outputScreen = document.getElementById('computer-player-output');
+const hitButton = document.getElementById('hit-button');
+const stayButton = document.getElementById('stay-button');
+const splitButton = document.getElementById('split-button');
+const ddownButton = document.getElementById('ddown-button');
+const bjExitButton = document.getElementById('bj-exit-button');
+
+
+class Card {
+
+}
+
+class Deck {
+
+}
+
+class Player {
+
+}
+
+class Jimbo {
+
+}
+
+class App {
+
+
+}
+
+console.log(hitButton);
+console.log(outputScreen)
